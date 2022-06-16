b2b021964065dbd340538f74f028f2d9cf69ecdd
Jonas Frey <jonfrey@ethz.ch>
2022-06-16 23:47:43 +0200

    gnn started implementing

--- wild_visual_navigation/feature_extractor/feature_extractor.py
+++ wild_visual_navigation/feature_extractor/feature_extractor.py
@@ -1,16 +1,16 @@
-import skimage
 from wild_visual_navigation.feature_extractor import StegoInterface
+import skimage
 
 
 class FeatureExtractor:
-    def __init__(self):
-        pass
+    def __init__(self, device):
+        self.si = StegoInterface(device=device)
 
     def extract(self, key, img, **kwargs):
-        getattr(self, key)(img, **kwargs)
+        return getattr(self, key)(img, **kwargs)
 
     def stego(self, img):
-        pass
+        return self.si.inference(img)
 
     def slic(self, img, n_segments=100, compactness=10.0):
         return skimage.segmentation.slic(img, n_segments=n_segments, compactness=compactness)
