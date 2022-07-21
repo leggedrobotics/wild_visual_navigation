ab3a38c2278f68ef6d33f29ba0369895b5349371
JonasFrey96 <JonasFrey96@users.noreply.github.com>
2022-07-21 14:23:54 +0000

    :art: Format Python code with psf/black
    Former-commit-id: d4d9b1c9a31be9bf6943ec7f8603b12a73a043db

--- wild_visual_navigation/utils/timing.py
+++ wild_visual_navigation/utils/timing.py
@@ -1,5 +1,6 @@
 import torch
 
+
 class Timer:
     def __init__(self, name="") -> None:
         self.name = name
@@ -9,25 +10,26 @@ class Timer:
         self.end = torch.cuda.Event(enable_timing=True)
         self.start.record()
         return "Hello, World!"
-    
+
     def __exit__(self, exc_type, exc_value, exc_tb):
         self.end.record()
         torch.cuda.synchronize()
         print(f"Time {self.name}: ", self.start.elapsed_time(self.end))
 
+
 if __name__ == "__main__":
-    
+
     print("Start timing using context manager")
 
     with Timer("Test1"):
-        s1,s2 = 1000, 1000
-        a = torch.zeros( (s1,s2))
+        s1, s2 = 1000, 1000
+        a = torch.zeros((s1, s2))
         for x in range(s1):
             for y in range(s2):
-                a[x,y] = x*y
+                a[x, y] = x * y
     with Timer("Test2"):
-        s1,s2 = 1000, 1000
-        a = torch.zeros( (s1,s2))
+        s1, s2 = 1000, 1000
+        a = torch.zeros((s1, s2))
         for x in range(s1):
             for y in range(s2):
-                a[y,x] = x*y
+                a[y, x] = x * y
