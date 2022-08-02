4a416a4a69747fec70563c6bbb5a72e90f9ba2b2
Jonas Frey <jonfrey@ethz.ch>
2022-08-02 13:02:11 +0200

    fixed minor bug

--- wild_visual_navigation_ros/scripts/wild_visual_navigation_node.py
+++ wild_visual_navigation_ros/scripts/wild_visual_navigation_node.py
@@ -329,9 +329,7 @@ class WvnRosInterface:
 
         # Publish predictions
         if mission_node is not None and self.run_online_learning:
-            np_prediction_image, np_uncertainty_image = self.traversability_estimator.plot_mission_node_prediction(
-                mission_node
-            )  # TODO: Check
+            np_prediction_image, np_uncertainty_image = self.traversability_estimator.plot_mission_node_prediction( mission_node )
             self.pub_image_prediction.publish(rc.numpy_to_ros_image(np_prediction_image))
             self.pub_image_prediction_uncertainty.publish(rc.numpy_to_ros_image(np_uncertainty_image))
 
