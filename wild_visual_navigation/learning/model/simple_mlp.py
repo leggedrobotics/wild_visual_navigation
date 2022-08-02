e556b22674f28170b4fbba132efb999cabed37c0
mmattamala <mmattamala@users.noreply.github.com>
2022-08-02 19:08:24 +0000

    :art: Format Python code with psf/black
    Former-commit-id: cbc0e38fe704e101c6c496b0639c3ae5e1cd6d58

--- wild_visual_navigation/learning/model/simple_mlp.py
+++ wild_visual_navigation/learning/model/simple_mlp.py
@@ -2,13 +2,14 @@ import torch
 from torch_geometric.data import Data
 import torch.nn.functional as F
 
+
 class SimpleMLP(torch.nn.Module):
     def __init__(self, input_size: int = 64, hidden_sizes: [int] = [255], reconstruction: bool = False):
         super(SimpleMLP, self).__init__()
         layers = []
         if reconstruction:
             hidden_sizes[-1] = hidden_sizes[-1] + input_size
-        
+
         for hs in hidden_sizes[:1]:
             layers.append(torch.nn.Linear(input_size, hs))
             layers.append(torch.nn.ReLU())
@@ -24,7 +25,7 @@ class SimpleMLP(torch.nn.Module):
         x = data.x
         # Checked data is correctly memory aligned and can be reshaped
         # If you change something in the dataloader make sure this is still working
-        x = x.reshape((data.batch.max().item()+1,-1,x.shape[1]))
+        x = x.reshape((data.batch.max().item() + 1, -1, x.shape[1]))
         x = self.layers(x)
-        x[:,:,0] = F.sigmoid(x[:,:,0])
-        return x.reshape( (-1, self.output_features))
+        x[:, :, 0] = F.sigmoid(x[:, :, 0])
+        return x.reshape((-1, self.output_features))
