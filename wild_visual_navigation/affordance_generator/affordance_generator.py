c94276d4ec5725dbf1b1d5a46edb3a138e4786f0
Matias Mattamala <matias@robots.ox.ac.uk>
2022-08-07 18:46:25 +0200

    Cleanup of affordance generator
    Former-commit-id: c7ae50e2a7a3c2664dae55bf5b084572cd871d07

--- wild_visual_navigation/affordance_generator/affordance_generator.py
+++ wild_visual_navigation/affordance_generator/affordance_generator.py
@@ -16,29 +16,84 @@ class AffordanceGenerator:
         Returns:
             None
         """
-        self.kalman_filter_ = KalmanFilter(dim_state=1, dim_control=1, dim_meas=1)
+        
+        self._kalman_filter_ = KalmanFilter(
+            dim_state=1, dim_control=1, dim_meas=1, outlier_rejection=False, outlier_delta=1.0, outlier_loss="huber"
+        )
+        self._kalman_filter_.init_process_model(proc_model=torch.eye(1) * 1, proc_cov=torch.eye(1) * 0.01)
+        self._kalman_filter_.init_meas_model(meas_model=torch.eye(1), meas_cov=torch.eye(1) * 100)
+
+        self._state = torch.FloatTensor([0.0])[None]
+        self._cov = torch.FloatTensor([0.1])[None]
 
     def update_with_velocities(
-        self, current_velocity: torch.tensor, target_velocity: torch.tensor, max_velocity: float = 1.0
+        self, current_velocity: torch.tensor, desired_velocity: torch.tensor, max_velocity: float = 1.0
     ):
         # Get dimensionality of input
         N = current_velocity.shape[-1]
 
-        # Compute velocity difference
-        error = torch.nn.functional.mse_loss(current_velocity, target_velocity) / max_velocity
+        # Compute discrepancy
+        # current_velocity = (current_velocity / max_velocity).clip(-1, 1)
+        # desired_velocity = (desired_velocity / max_velocity).clip(-1, 1)
+        # raw_error = (torch.nn.functional.mse_loss(current_velocity, desired_velocity))  / 2
+
+        raw_error = ((torch.nn.functional.mse_loss(current_velocity, desired_velocity)) / max_velocity).clip(0, 1)
+
+        # Filtering stage
+        self._state, self._cov = self._kalman_filter_(self._state, self._cov, raw_error)
 
         # Clip output
-        self._affordance = 1.0 - error.clip(0, 1)
-        self._affordance_var = torch.FloatTensor(1).to(current_velocity.device)
+        s = nn.Sigmoid()
+        self._affordance = 1.0 - self._state  # Clipping the output of the KF is a bit sketchy
+        self._affordance_conf = torch.tensor([0.9]) # 1.0 - self._cov
 
-        return self._affordance, self._affordance_var
+        # Return
+        return self._affordance, self._affordance_conf, raw_error
 
 
 def run_affordance_generator():
     """Projects 3D points to example images and returns an image with the projection"""
 
+    from wild_visual_navigation.learning.dataset import TwistDataset
     import matplotlib.pyplot as plt
+    import pandas as pd
+
+    # Prepare dataset
+    root = str(os.path.join(WVN_ROOT_DIR, "results/arche_loop"))
+    current_filename = "current_robot_twist.csv"
+    desired_filename = "desired_robot_twist.csv"
+    data = TwistDataset(root, current_filename, desired_filename, seq_size=1, velocities=["vx", "vy"])
+
+    # Prepare affordance generator
+    ag = AffordanceGenerator()
+
+    # Saved data list
+    saved_data = []
+
+    # Iterate the dataset
+    for i in range(data.size):
+        # Get samples
+        t, curr, des = data[i]
+
+        # Update affordance
+        aff, aff_cov, raw_error = ag.update_with_velocities(curr, des, max_velocity=0.8)
+        saved_data.append([t.item(), curr.norm().item(), des.norm().item(), aff.item(), aff_cov.item(), raw_error.item()])
+
+    df = pd.DataFrame(saved_data, columns=["ts", "curr", "des", "aff", "aff_cov", "raw_err"])
+
+    df["aff_upper"] = df["aff"] + df["aff_cov"]
+    df["aff_lower"] = df["aff"] - df["aff_cov"]
+    plt.plot(df["ts"], df["curr"], label="Current twist", color="b")
+    plt.plot(df["ts"], df["des"], label="Desired twist", color="k")
+    plt.plot(df["ts"], df["aff"], label="Affordance", color="r")
+    plt.plot(df["ts"], df["aff_cov"], label="Affordance Cov", color="m")
+    # plt.plot(df["ts"], df["raw_err"], label="Raw error", color="g")
+    # plt.plot(df["ts"], (1.0 - df["aff"]) - df["raw_err"], label="Affordance", color="c")
+    # plt.fill_between(df["ts"], 0.0, df["aff_cov"], alpha=0.2, label="Aff bounds (1$\sigma$)", color="r")
+    # plt.fill_between(df["ts"], df["aff_lower"], df["aff_upper"], alpha=0.3, label="Aff bounds (1$\sigma$)", color="r")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
-    run_image_projector()
+    run_affordance_generator()
