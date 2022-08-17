a83458b5042ce74406b829b700906ed2c48530c9
Matias Mattamala <matias@robots.ox.ac.uk>
2022-08-17 16:55:15 +0200

    First attempt to integrate stego segments, still debugging

--- tests/test_feature_extractor.py
+++ tests/test_feature_extractor.py
@@ -1,5 +1,7 @@
-from wild_visual_navigation.feature_extractor import FeatureExtractor
 from wild_visual_navigation import WVN_ROOT_DIR
+from wild_visual_navigation.feature_extractor import FeatureExtractor
+from wild_visual_navigation.utils import get_img_from_fig
+import matplotlib.pyplot as plt
 import torch
 from torchvision import transforms as T
 from pathlib import PurePath, Path
@@ -21,12 +23,22 @@ def test_feature_extractor():
     np_img = cv2.imread(os.path.join(WVN_ROOT_DIR, "assets/images/forest_clean.png"))
     img = torch.from_numpy(cv2.cvtColor(np_img, cv2.COLOR_BGR2RGB)).to(device)
     img = img.permute(2, 0, 1)
-
     img = (img.type(torch.float32) / 255)[None]
     adj, feat, seg, center = fe.extract(transform(img.clone()))
 
     p = PurePath(WVN_ROOT_DIR).joinpath("results", "test_feature_extractor", "forest_clean_graph.png")
     Path(p.parent).mkdir(parents=True, exist_ok=True)
+
+    # Plot result as in colab
+    fig, ax = plt.subplots(1, 2, figsize=(5 * 3, 5))
+
+    ax[0].imshow(transform(img).permute(0, 2, 3, 1)[0].cpu())
+    ax[0].set_title("Image")
+    ax[1].imshow(seg.cpu())
+    ax[1].set_title("Segmentation")
+
+    # Store results to test directory
+    img = get_img_from_fig(fig)
     img.save(str(p))
 
 
--- wild_visual_navigation/feature_extractor/feature_extractor.py
+++ wild_visual_navigation/feature_extractor/feature_extractor.py
@@ -118,10 +118,14 @@ class FeatureExtractor:
     def segment_stego(self, img, **kwargs):
         # Prepare input image
         img_internal = img.clone()
-        self.extractor.inference(img_internal)
-        seg = self.extractor.segments
+        self.extractor.inference_crf(img_internal)
+        seg = self.extractor.linear_segments
 
-        assert False, "We need to make the segments as in SLIC, applying the linear probe"
+        # we are going to combine the channels in a single image to define a "key" for each segment
+        seg = torch.from_numpy(seg[:, :, 0] * 1000000 + seg[:, :, 1] * 1000 + seg[:, :, 2])
+        for i, k in enumerate(seg.unique()):
+            seg[seg == k.item()] = i
+        seg = seg.to(self.device)
 
         # Extract adjacency_list based on segments
         edges = self.segment_extractor.adjacency_list(seg[None, None])
@@ -163,11 +167,13 @@ class FeatureExtractor:
             features = self.extractor(img)
         return features
 
+    @torch.no_grad()
     def compute_dino(self, img: torch.tensor, seg: torch.tensor, center: torch.tensor, **kwargs):
         img_internal = img.clone()
         features = self.extractor.inference(img_internal)
         return features
 
+    @torch.no_grad()
     def compute_stego(self, img: torch.tensor, seg: torch.tensor, center: torch.tensor, **kwargs):
         return self.extractor.features
 
@@ -184,116 +190,116 @@ class FeatureExtractor:
         else:
             return dense_features
 
-    @torch.no_grad()
-    def dino_slic(
-        self,
-        img: torch.tensor,
-        n_segments: int = 200,
-        compactness: float = 10.0,
-        return_centers: bool = False,
-        return_image: bool = False,
-        show: bool = False,
-    ):
-        """Extract dino feature form image
+    # @torch.no_grad()
+    # def dino_slic(
+    #     self,
+    #     img: torch.tensor,
+    #     n_segments: int = 200,
+    #     compactness: float = 10.0,
+    #     return_centers: bool = False,
+    #     return_image: bool = False,
+    #     show: bool = False,
+    # ):
+    #     """Extract dino feature form image
 
-        Args:
-            img (torch.Tensor, dtype=torch.float32, shape=(BS, C, H, W)): Image
-            n_segments (int, optional): Defaults to 100.
-            compactness (float, optional): Defaults to 100.0.
-            return_centers (bool, optional): Defaults to False.
-            return_image (bool, optional): Defaults to False.
-            show (bool, optional): Defaults to False.
+    #     Args:
+    #         img (torch.Tensor, dtype=torch.float32, shape=(BS, C, H, W)): Image
+    #         n_segments (int, optional): Defaults to 100.
+    #         compactness (float, optional): Defaults to 100.0.
+    #         return_centers (bool, optional): Defaults to False.
+    #         return_image (bool, optional): Defaults to False.
+    #         show (bool, optional): Defaults to False.
 
-        Returns:
-            adj (torch.Tensor, dtype=torch.uint32, shape=(2,N)): Graph Structure
-            feat (torch.Tensor, dtype=torch.float32, shape=(N, C)): Features
-            seg (torch.Tensor, dtype=torch.uint32, shape=(H, W)): Segmentation
-            center (torch.Tensor, dtype=torch.float32, shape=(2,N)): Center of custer in image plane
-        """
+    #     Returns:
+    #         adj (torch.Tensor, dtype=torch.uint32, shape=(2,N)): Graph Structure
+    #         feat (torch.Tensor, dtype=torch.float32, shape=(N, C)): Features
+    #         seg (torch.Tensor, dtype=torch.uint32, shape=(H, W)): Segmentation
+    #         center (torch.Tensor, dtype=torch.float32, shape=(2,N)): Center of custer in image plane
+    #     """
 
-        # Currently on BS=1 supported
-        assert img.shape[0] == 1 and len(img.shape) == 4
+    #     # Currently on BS=1 supported
+    #     assert img.shape[0] == 1 and len(img.shape) == 4
 
-        # Prepare input image
-        img_internal = img.clone()
-        # img_internal = self.crop(img_internal.to(self.device))
-        img_internal = self.norm(img_internal)
+    #     # Prepare input image
+    #     img_internal = img.clone()
+    #     # img_internal = self.crop(img_internal.to(self.device))
+    #     img_internal = self.norm(img_internal)
 
-        # Extract dino features
-        feat_dino = self.extractor.inference(img_internal, interpolate=False)
+    #     # Extract dino features
+    #     feat_dino = self.extractor.inference(img_internal, interpolate=False)
 
-        # Fix size of DINO features to match input image's size
-        B, D, H, W = img.shape
-        new_size = (H, H)
-        pad = int((W - H) / 2)
-        feat_dino = F.interpolate(feat_dino, new_size, mode="bilinear", align_corners=True)
-        feat_dino = F.pad(feat_dino, pad=[pad, pad, 0, 0])
+    #     # Fix size of DINO features to match input image's size
+    #     B, D, H, W = img.shape
+    #     new_size = (H, H)
+    #     pad = int((W - H) / 2)
+    #     feat_dino = F.interpolate(feat_dino, new_size, mode="bilinear", align_corners=True)
+    #     feat_dino = F.pad(feat_dino, pad=[pad, pad, 0, 0])
 
-        # Get slic clusters
-        img_np = kornia.utils.tensor_to_image(img)
-        seg = skimage.segmentation.slic(
-            img_np, n_segments=n_segments, compactness=compactness, start_label=0, channel_axis=2
-        )
+    #     # Get slic clusters
+    #     img_np = kornia.utils.tensor_to_image(img)
+    #     seg = skimage.segmentation.slic(
+    #         img_np, n_segments=n_segments, compactness=compactness, start_label=0, channel_axis=2
+    #     )
 
-        # extract adjacency_list based on clusters
-        seg = torch.from_numpy(seg).to(self.device)
-        adjacency_list = self.se.adjacency_list(seg[None, None])
+    #     # extract adjacency_list based on clusters
+    #     seg = torch.from_numpy(seg).to(self.device)
+    #     adjacency_list = self.se.adjacency_list(seg[None, None])
 
-        # get mean dino features for each cluster
-        features = []
-        for i in range(seg.max() + 1):
-            m = seg == i
-            x, y = torch.where(m)
-            feat = feat_dino[0, :, x, y].median(dim=1)[0]
-            features.append(feat)
+    #     # get mean dino features for each cluster
+    #     features = []
+    #     for i in range(seg.max() + 1):
+    #         m = seg == i
+    #         x, y = torch.where(m)
+    #         feat = feat_dino[0, :, x, y].median(dim=1)[0]
+    #         features.append(feat)
 
-        ret = (adjacency_list.T, torch.stack(features, dim=1).T, seg)
+    #     ret = (adjacency_list.T, torch.stack(features, dim=1).T, seg)
 
-        if return_centers:
-            ret += (self.se.centers(seg[None, None]),)
+    #     if return_centers:
+    #         ret += (self.se.centers(seg[None, None]),)
 
-        if show or return_image:
-            ph = PlotHelper()
-            ph.add(img_np, "Input Image Cropped")
-            ph.add(seg, "SLIC Segmentation")
+    #     if show or return_image:
+    #         ph = PlotHelper()
+    #         ph.add(img_np, "Input Image Cropped")
+    #         ph.add(seg, "SLIC Segmentation")
 
-            img_pil = Image.fromarray(np.uint8(img_np * 255))
-            seg_pil = Image.fromarray(np.uint8(seg.cpu().numpy()[0, 0]))
-            img_draw = ImageDraw.Draw(img_pil)
-            seg_draw = ImageDraw.Draw(seg_pil)
+    #         img_pil = Image.fromarray(np.uint8(img_np * 255))
+    #         seg_pil = Image.fromarray(np.uint8(seg.cpu().numpy()[0, 0]))
+    #         img_draw = ImageDraw.Draw(img_pil)
+    #         seg_draw = ImageDraw.Draw(seg_pil)
 
-            centers = self.se.centers(seg)
-            fil_col = (seg.max() + 5).item()
-            for i in range(adjacency_list.shape[1]):
-                a, b = adjacency_list[0, i], adjacency_list[1, i]
-                line_params = centers[a].tolist() + centers[b].tolist()
-                img_draw.line(line_params, fill=fil_col)
-                seg_draw.line(line_params, fill=fil_col)
+    #         centers = self.se.centers(seg)
+    #         fil_col = (seg.max() + 5).item()
+    #         for i in range(adjacency_list.shape[1]):
+    #             a, b = adjacency_list[0, i], adjacency_list[1, i]
+    #             line_params = centers[a].tolist() + centers[b].tolist()
+    #             img_draw.line(line_params, fill=fil_col)
+    #             seg_draw.line(line_params, fill=fil_col)
 
-            for i in range(centers.shape[0]):
-                params = centers[i].tolist()
-                params = [p - 2 for p in params] + [p + 2 for p in params]
-                img_draw.ellipse(params, width=10, fill=fil_col + 1)
-                seg_draw.ellipse(params, width=10, fill=fil_col + 1)
+    #         for i in range(centers.shape[0]):
+    #             params = centers[i].tolist()
+    #             params = [p - 2 for p in params] + [p + 2 for p in params]
+    #             img_draw.ellipse(params, width=10, fill=fil_col + 1)
+    #             seg_draw.ellipse(params, width=10, fill=fil_col + 1)
 
-            ph.add(np.array(img_pil), "Image Feature-Graph")
-            ph.add(np.array(seg_pil), "SLIC Feature-Graph")
+    #         ph.add(np.array(img_pil), "Image Feature-Graph")
+    #         ph.add(np.array(seg_pil), "SLIC Feature-Graph")
 
-            if show:
-                ph.show()
+    #         if show:
+    #             ph.show()
 
-            if return_image:
-                ret += (ph.get_img(),)
+    #         if return_image:
+    #             ret += (ph.get_img(),)
 
-        return ret
+    #     return ret
 
-    def stego(self, img: torch.tensor):
-        # currently on BS=1 supported
-        assert img.shape[0] == 1 and len(img.shape) == 4
+    # def stego(self, img: torch.tensor):
+    #     # currently on BS=1 supported
+    #     assert img.shape[0] == 1 and len(img.shape) == 4
 
-        # Prepare input image
-        img_internal = img.clone()
-        # img_internal = self.crop(img_internal.to(self.device))
-        img_internal = self.norm(img_internal)
+    #     # Prepare input image
+    #     img_internal = img.clone()
+    #     # img_internal = self.crop(img_internal.to(self.device))
+    #     img_internal = self.norm(img_internal)
 
-        return self.extractor.inference(img_internal)
+    #     return self.extractor.inference(img_internal)
