f503cdb5ee60aeb9016c5577c1f4352d2e1995cc
Matias Mattamala <matias@robots.ox.ac.uk>
2022-09-07 12:21:45 +0200

    Fix image_projector to use batches
    Former-commit-id: db67505c370cbbf4500172ad94af62df9109b001

--- wild_visual_navigation/utils/__init__.py
+++ wild_visual_navigation/utils/__init__.py
@@ -1,4 +1,4 @@
-from .meshes import make_box, make_rounded_box, make_ellipsoid, make_plane, make_polygon_from_points
+from .meshes import make_box, make_rounded_box, make_ellipsoid, make_plane, make_polygon_from_points, make_dense_plane
 from .timing import Timer, accumulate_time
 from .klt_tracker import KLTTracker, KLTTrackerOpenCV
 from .confidence_generator import ConfidenceGenerator
