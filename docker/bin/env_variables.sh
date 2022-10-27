4bf4c34eac621c5a59f4dbcf0888c2988a30482b
Matias Mattamala <matias@robots.ox.ac.uk>
2022-10-27 19:23:46 +0100

    Reorganize files and scripts to avoid redundancies

--- docker/bin/env_variables.sh
+++ docker/bin/env_variables.sh
@@ -14,6 +14,6 @@ WVN_JETSON_TAG="rslethz/jetpack-5:r34.1.1-wvn"
 WVN_DESKTOP_TAG="rslethz/desktop:r34.1.1-wvn"
 
 # ORI-compliant tags for built images
-ORI_PYG_ORI_JETSON_TAG="ori-ci-gateway.robots.ox.ac.uk:12002/drs/jetson:r34.1.1-ml-pyg-latest"
+ORI_PYG_JETSON_TAG="ori-ci-gateway.robots.ox.ac.uk:12002/drs/jetson:r34.1.1-ml-pyg-latest"
 ORI_WVN_JETSON_TAG="ori-ci-gateway.robots.ox.ac.uk:12002/drs/jetson:r34.1.1-wvn-latest"
 ORI_WVN_DESKTOP_TAG="ori-ci-gateway.robots.ox.ac.uk:12002/drs/desktop:wvn-latest"
