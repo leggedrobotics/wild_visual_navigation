127cf71670a419bdf536fa63397d3b4603cebf44
Matias Mattamala <matias@robots.ox.ac.uk>
2022-11-03 13:35:27 +0100

    Minor fixes

--- docker/bin/run.sh
+++ docker/bin/run.sh
@@ -29,7 +29,7 @@ if [[ "$TARGET" == "jetson" ]]; then
     docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v ${GIT_WS}:/root/git -v ${CATKIN_WS}:/root/catkin_ws/ $WVN_JETSON_TAG
 
 elif [[ "$TARGET" == "desktop" ]]; then
-    docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v ${GIT_WS}:/root/git -v ${CATKIN_WS}:/root/catkin_ws/ $WVN_DESKTOP_TAG
+    docker run -it --rm --net=host --gpus all --runtime nvidia -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v ${GIT_WS}:/root/git -v ${CATKIN_WS}:/root/catkin_ws/ $WVN_DESKTOP_TAG
 
 else
     echo "Error: unsupported target [$TARGET]"
