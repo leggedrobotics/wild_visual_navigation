1fb86e215f98d0af1c424c0da5d8130c26988d85
Matias Mattamala <matias@robots.ox.ac.uk>
2023-01-17 10:34:22 +0000

    Confidence generator fixes
    Former-commit-id: 0f256e42341dd8f48a1e0aea8c8e20383464b2fb

--- tests/test_confidence_generator.py
+++ tests/test_confidence_generator.py
@@ -2,6 +2,7 @@ from wild_visual_navigation.utils import ConfidenceGenerator
 import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
+import numpy as np
 
 
 def generate_traversability_test_signal(N=500, T=10, events=[3, 8], event_length=0.5, device=torch.device("cpu")):
@@ -22,6 +23,7 @@ def generate_traversability_test_signal(N=500, T=10, events=[3, 8], event_length
 def test_confidence_generator():
     device = torch.device("cpu")
     N = 1000
+    sigma_factor = 10.0
 
     # Design a long 1D traversability signal
     t, x = generate_traversability_test_signal(N=N, T=30, events=[3, 12], event_length=[5.0, 10], device=device)
@@ -36,7 +38,7 @@ def test_confidence_generator():
     salt_pepper_noise[torch.logical_and(salt_pepper_noise > min_v, salt_pepper_noise < max_v)] = 0.0
 
     # White noise
-    white_noise = 0.3 * (torch.rand(x.shape, device=device) - 0.5)
+    white_noise = 0.1 * (torch.rand(x.shape, device=device) - 0.5)
 
     # Add noise to signal
     ws = 0.0  # salt and pepper noise weight
@@ -49,117 +51,98 @@ def test_confidence_generator():
     x_noisy_positive[~x_is_positive] = torch.nan
 
     # Add more noise to simulate predictions we don't know
-    more_noise = torch.rand(x.shape, device=device) - 0.5
+    more_noise = 1.0 * (torch.rand(x.shape, device=device) - 0.5)
     x_noisy += more_noise * ~x_is_positive
 
+    # Add constant bias
+    bias = t * 0.1
+    x += bias
+    x_noisy += bias
+    x_noisy_positive += bias
+
     # Arrays to store the predictions
-    x_mean = torch.zeros((3, N), device=device)
-    x_std = torch.zeros((3, N), device=device)
-    x_conf = torch.zeros((3, N), device=device)
+    loss = torch.zeros((3, N), device=device)
+    loss_mean = torch.zeros((3, N), device=device)
+    loss_std = torch.zeros((3, N), device=device)
+    conf = torch.zeros((3, N), device=device)
 
     # Naive confidence generator
-    cg = ConfidenceGenerator().to(device)
-    # Confidence generator for positive samples
-    cg2 = ConfidenceGenerator().to(device)
+    cg = ConfidenceGenerator(std_factor=sigma_factor).to(device)
 
     # Run
     for i in range(x.shape[0]):
         # Simulate reconstruccion error
         s = F.mse_loss(x_noisy[i], x[i])
         is_pos = x_is_positive[i]
-
-        # Run confidence generator
-        conf = cg.update(s)
-        # Get mean and confidence
-        x_mean[0, i] = cg.mean[0]
-        x_std[0, i] = cg.std[0]
-        x_conf[0, i] = conf[0]
+        loss[0, i] = s
 
         # Run confidence generator
         if is_pos:
-            # Get mean and confidence
-            conf2 = cg2.update(s)
-            x_mean[1, i] = cg2.mean[0]
-            x_std[1, i] = cg2.std[0]
-            x_conf[1, i] = conf2[0]
+            c = cg.update(s, s)
+        else:
+            c = cg.update(s, torch.tensor([]))  # masking tensors returns empty tensor
 
-            # Get confidence from sigmoid method
-            conf = cg2.update_sigmoid(s, slope=1, cutoff=0.2)
-            x_conf[2, i] = conf[0]
+        # Get mean and confidence
+        loss_mean[0, i] = cg.mean[0]
+        loss_std[0, i] = cg.std[0]
+        conf[0, i] = c[0]
 
     # Convert to numpy
     t_np = t.cpu().numpy()
+    loss_np = loss.cpu().numpy()
     x_np = x.cpu().numpy()
     x_noisy_np = x_noisy.cpu().numpy()
     x_noisy_positive_np = x_noisy_positive.cpu().numpy()
-    x_mean_np = x_mean.detach().cpu().numpy()
-    x_std_np = x_std.detach().cpu().numpy()
-    x_conf_np = x_conf.detach().cpu().numpy()
+    loss_mean_np = loss_mean.detach().cpu().numpy()
+    loss_std_np = loss_std.detach().cpu().numpy()
+    conf_np = conf.detach().cpu().numpy()
 
     # Plot
-    fig, axs = plt.subplots(4, sharex=True)
+    fig, axs = plt.subplots(3, sharex=True)
     fig.suptitle("Confidence generator")
 
     # Signal subplot
-    axs[0].plot(t_np, x_np, label="Target signal", color="k")
     axs[0].plot(t_np, x_noisy_np, label="Reconstructed signal", color="r")
-    axs[0].plot(t_np, x_noisy_positive_np, label="Noisy signal - positive samples", color=(0.5, 0, 0), marker=".")
+    axs[0].plot(t_np, x_np, label="Target signal", color="k")
+    axs[0].plot(t_np, x_noisy_positive_np, label="Positive samples", color=(0.5, 0, 0), marker=".")
     axs[0].set_ylabel("Signals")
     axs[0].legend(loc="upper right")
 
-    # Confidence subplot
-    axs[1].plot(t_np, x_mean_np[0], label="Confidence mean", color="b")
+    # Naive loss distribution mean
     axs[1].fill_between(
         t_np,
-        x_mean_np[0] - x_std_np[0],
-        x_mean_np[0] + x_std_np[0],
-        alpha=0.3,
-        label="Confidence mean (1$\sigma$)",
+        loss_mean_np[0] - sigma_factor * loss_std_np[0],
+        loss_mean_np[0] + sigma_factor * loss_std_np[0],
+        alpha=0.2,
+        label=f"Loss mean $\pm{sigma_factor}\sigma$",
         color="b",
     )
-    axs[1].plot(t_np, x_conf_np[0], label="Confidence", color="k")
+
     axs[1].fill_between(
         t_np,
-        0,
-        x_conf_np[0],
-        alpha=0.3,
-        color="k",
+        loss_mean_np[0] - loss_std_np[0],
+        loss_mean_np[0] + loss_std_np[0],
+        alpha=0.6,
+        label="Loss mean $\pm\sigma$",
+        color="b",
     )
-    axs[1].set_ylabel("Confidence")
+
+    axs[1].plot(t_np, loss_np[0], label="Loss", color="k")
+    axs[1].plot(t_np, loss_mean_np[0], label="Loss mean", color="b")
+    axs[0].set_ylabel("Loss distribution")
     axs[1].legend(loc="upper right")
 
-    # Confidence subplot
-    axs[2].plot(t_np, x_mean_np[1], label="Filtered mean - positive", color="m")
-    axs[2].fill_between(
-        t_np,
-        x_mean_np[1] - x_std_np[1],
-        x_mean_np[1] + x_std_np[1],
-        alpha=0.3,
-        label="Filtered mean (1$\sigma$) - positive",
-        color="m",
-    )
-    axs[2].plot(t_np, x_conf_np[1], label="Confidence2", color="k")
+    # Naive Confidence
+    axs[2].plot(t_np, conf_np[0], label="Confidence", color="k")
     axs[2].fill_between(
         t_np,
         0,
-        x_conf_np[1],
+        conf_np[0],
         alpha=0.3,
         color="k",
     )
-    axs[2].set_ylabel("Confidence2")
+    axs[2].set_ylabel("Confidence")
     axs[2].legend(loc="upper right")
-
-    # COnfidence suplot sigmoid
-    axs[3].plot(t_np, x_conf_np[2], label="Confidence3", color="k")
-    axs[3].fill_between(
-        t_np,
-        0,
-        x_conf_np[2],
-        alpha=0.3,
-        color="k",
-    )
-    axs[3].set_ylabel("Confidence - sigmoid")
-    axs[3].legend(loc="upper right")
 
     plt.show()
 
