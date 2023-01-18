36eb45606f39ee91044083b4178005622e2d9dd4
Jonas Frey <jonfrey@ethz.ch>
2023-01-18 15:09:04 +0100

    removed default rosparam

--- scripts/dataset_generation/extract_images_and_labels.py
+++ scripts/dataset_generation/extract_images_and_labels.py
@@ -99,15 +99,15 @@ def do(n, dry_run):
     mission = s.split("/")[-1]
 
     # 2022-05-12T11:56:13_mission_0_day_3
-    # extraction_store_foler = f"/media/Data/Datasets/2022_Perugia/wvn_output/day3/{mission}"
-    extraction_store_foler = f"/media/matias/datasets/2022_Perugia/wvn_output/day3/{mission}"
+    # extraction_store_folder = f"/media/Data/Datasets/2022_Perugia/wvn_output/day3/{mission}"
+    extraction_store_folder = f"/media/matias/datasets/2022_Perugia/wvn_output/day3/{mission}"
 
-    if os.path.exists(extraction_store_foler):
-        print(f"Stopped because folder already exists: {extraction_store_foler}")
+    if os.path.exists(extraction_store_folder):
+        print(f"Stopped because folder already exists: {extraction_store_folder}")
         return
 
     rosparam.set_param("wild_visual_navigation_node/mode", "extract_labels")
-    rosparam.set_param("wild_visual_navigation_node/extraction_store_foler", extraction_store_foler)
+    rosparam.set_param("wild_visual_navigation_node/extraction_store_folder", extraction_store_folder)
 
     # for proprioceptive callback
     state_msg_valid = False
