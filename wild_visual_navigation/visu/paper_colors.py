96d6237d17a451df0c9c5f372065e5b31bdefa82
Matias Mattamala <matias@robots.ox.ac.uk>
2023-01-21 15:49:13 +0000

    Runtime fixes

--- wild_visual_navigation/visu/paper_colors.py
+++ wild_visual_navigation/visu/paper_colors.py
@@ -25,6 +25,7 @@ def adjust_lightness(color, amount=0.5):
     """
     import matplotlib.colors as mc
     import colorsys
+
     try:
         c = mc.cnames[color]
     except:
@@ -32,16 +33,16 @@ def adjust_lightness(color, amount=0.5):
     c = colorsys.rgb_to_hls(*mc.to_rgb(c))
     return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
 
+
 def darken(color, amount=0.0):
-    """ Darkens a color. 0.0 means no change
-    """
+    """Darkens a color. 0.0 means no change"""
     import numpy as np
+
     return adjust_lightness(color, 1.0 - np.clip(amount, 0.0, 1.0))
 
+
 def lighten(color, amount=0.0):
-    """ Darkens a color. 0.0 means no change
-    """
+    """Darkens a color. 0.0 means no change"""
     import numpy as np
+
     return adjust_lightness(color, 1.0 + np.clip(amount, 0.0, 1.0))
-
-
