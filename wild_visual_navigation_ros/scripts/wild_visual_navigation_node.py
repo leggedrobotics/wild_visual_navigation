fixed trav scaling auroc computation

wild_visual_navigation_ros/scripts/wild_visual_navigation_node.py
@@ -690,7 +690,7 @@ class WvnRosInterface:
             # Optionally rescale the traversability output before publishing
             if self.scale_traversability:
                 # Compute ROC Threshold
-                try:
+                if self.traversability_estimator._auxilary_training_roc._update_count != 0:
                     fpr, tpr, thresholds = self.traversability_estimator._auxilary_training_roc.compute()
                     index = torch.where(fpr > self.scale_traversability_max_fpr)[0][0]
                     threshold = thresholds[index]
@@ -704,8 +704,6 @@ class WvnRosInterface:
                     traversability[~m] *= 0.5 / (1 - threshold)
                     traversability[~m] += 0.5
                     traversability.clip(0, 1)
-                except:
-                    print("Failed to scale output image. Most likely due to ROC computation failed")
 
             out_trav = traversability[fs]
             out_conf = mission_node.confidence[fs]