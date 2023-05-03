9eb6b26f1501a3aec9425ef6a63cd473d39afbf1
Matias Mattamala <matias@robots.ox.ac.uk>
2023-05-03 07:51:15 +0100

    Robot changes to enable HDR camera

--- wild_visual_navigation_ros/scripts/rotate_image.py
+++ wild_visual_navigation_ros/scripts/rotate_image.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 import rospy
+import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import cv2
+
 
 
 class ImageRotator:
     def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/hdr_cam/image", Image, self.image_callback)
-        self.image_pub = rospy.Publisher("/hdr_cam/rotated/image", Image, queue_size=1)
+        self.image_sub = rospy.Subscriber("/hdr_cam_driver/image", Image, self.image_callback)
+        self.image_pub = rospy.Publisher("/hdr_cam_driver/rotated/image", Image, queue_size=10)
 
     def image_callback(self, data):
         try:
             # Convert ROS image message to OpenCV image
-            cv_image = self.bridge.imgmsg_to_cv2(data, "passthrough")
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             rospy.logerr("CvBridgeError: {0}".format(e))
             return
@@ -25,7 +26,7 @@ class ImageRotator:
 
         try:
             # Convert OpenCV image to ROS image message
-            out_msg = self.bridge.cv2_to_imgmsg(rotated_image, "passthrough")
+            out_msg = self.bridge.cv2_to_imgmsg(rotated_image, "bgr8")
             out_msg.header = data.header
             self.image_pub.publish(out_msg)
         except CvBridgeError as e:
