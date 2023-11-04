56b5fa2b88b94f9c9e4e2f583d90a0ba29dbe7dc
swsychen <swsychen@gmail.com>
2023-11-04 23:11:47 +0100

    define decoderoutput msg

--- BaseWVN/utils/NodeForROS.py
+++ BaseWVN/utils/NodeForROS.py
@@ -115,7 +115,7 @@ class NodeForROS:
             raise ValueError(f"Couldn't find tf between {parent_frame} and {child_frame} in the provided message")
 
     def shutdown_callback(self, *args, **kwargs):
-
+        print(f"Node killed {args}")
         rospy.signal_shutdown(f"Node killed {args}")
         sys.exit(0)
     
--- src/wild_visual_navigation_ros/scripts/Phy_decoder_node.py
+++ src/wild_visual_navigation_ros/scripts/Phy_decoder_node.py
@@ -8,19 +8,19 @@ from Phy_Decoder import initialize_models,prepare_padded_input,RNNInputBuffer
 from pytictac import ClassTimer, ClassContextTimer, accumulate_time
 import ros_converter as rc
 from anymal_msgs.msg import AnymalState
-from geometry_msgs.msg import PoseStamped, Point, TwistStamped
+from geometry_msgs.msg import Pose, Point, Quaternion
 from rosgraph_msgs.msg import Clock
 from nav_msgs.msg import Path
 from sensor_msgs.msg import Image, CameraInfo, CompressedImage
 from std_msgs.msg import ColorRGBA, Float32, Float32MultiArray,Header
 from threading import Thread, Event
 from visualization_msgs.msg import Marker
-# from wild_visual_navigation_msgs.msg import StampedFloat32MultiArray
+from wild_visual_navigation_msgs.msg import PhyDecoderOutput
 import message_filters
 import os
 import rospy
 import seaborn as sns
-
+import tf.transformations as tft
 import torch
 import numpy as np
 from typing import Optional
@@ -72,10 +72,10 @@ class PhyDecoder(NodeForROS):
 
 
         # Results publisher
-        phy_val_pub=rospy.Publisher('/vd_pipeline/phy_val_pred', Float32MultiArray, queue_size=10)
+        phy_decoder_pub=rospy.Publisher('/vd_pipeline/phy_decoder_out', PhyDecoderOutput, queue_size=10)
         # stamped_debug_info_pub=rospy.Publisher('/stamped_debug_info', StampedFloat32MultiArray, queue_size=10)
         # Fill in handler
-        self.decoder_handler['phy_val_pub']=phy_val_pub
+        self.decoder_handler['phy_decoder_pub']=phy_decoder_pub
 
     
         
@@ -86,7 +86,8 @@ class PhyDecoder(NodeForROS):
         """
         self.step+=1
         self.system_events["state_callback_received"] = {"time": rospy.get_time(), "value": "message received"}
-        
+        msg=PhyDecoderOutput()
+        msg.header=anymal_state_msg.header
         try:
 
             # Query footprint transform from AnymalState message
@@ -102,6 +103,7 @@ class PhyDecoder(NodeForROS):
             
             # Query 4 feet transforms from AnymalState message
             pose_feet_in_world = {}
+            foot_poses=[]
             for foot in self.feet_list:
                 suc, pose_foot_in_world = rc.ros_tf_to_torch(
                     self.query_tf(self.fixed_frame, foot, from_message=anymal_state_msg), device=self.device
@@ -113,6 +115,9 @@ class PhyDecoder(NodeForROS):
                     }
                     return
                 pose_feet_in_world[foot] = pose_foot_in_world
+                foot_pose=self.matrix_to_pose(pose_foot_in_world.cpu().numpy())
+                foot_poses.append(foot_pose)
+            msg.feet_poses=foot_poses
 
             """ 
             Fric/Stiff-Decoder input topics & prediction
@@ -135,7 +140,8 @@ class PhyDecoder(NodeForROS):
             # pub fric and stiff together
                 new_priv=torch.cat([fric_pred,stiff_pred],dim=-1)
                 new_priv=new_priv[:,-1,:].squeeze(0).cpu().numpy()
-                self.decoder_handler['phy_val_pub'].publish(Float32MultiArray(data=new_priv))
+                msg.prediction=new_priv
+                # self.decoder_handler['phy_decoder_pub'].publish(Float32MultiArray(data=new_priv))
 
 
             pass
@@ -148,6 +154,21 @@ class PhyDecoder(NodeForROS):
                 "value": f"failed to execute {e}",
             }
         pass
+
+    @accumulate_time
+    def matrix_to_pose(self,matrix):
+        # Extract the translation (last column of the matrix)
+        position = Point(*matrix[:3, 3])
+
+        # Extract the rotation (upper-left 3x3 submatrix) and convert to quaternion
+        quaternion = Quaternion(*tft.quaternion_from_matrix(matrix))
+
+        # Create a Pose message and assign the position and orientation
+        pose_msg = Pose()
+        pose_msg.position = position
+        pose_msg.orientation = quaternion
+
+        return pose_msg
 
  
 if __name__ == "__main__":
