318b4cad7f71559a167a3d34b65a0ef1be53998a
swsychen <swsychen@gmail.com>
2023-11-07 12:38:08 +0100

    base feat_ext

--- BaseWVN/utils/dinov2_interface.py
+++ BaseWVN/utils/dinov2_interface.py
@@ -7,9 +7,6 @@ class Dinov2Interface:
     def __init__(
         self,
         device: str,
-        input_size: int = 448,
-        original_size: Tuple[int, int] = (1920, 1280),
-        input_interp: str = "bilinear",
         model_type: str = "vit_small",
         **kwargs
     ):
@@ -28,50 +25,18 @@ class Dinov2Interface:
         elif self._model_type == "vit_huge":
             self.model=torch.hub.load('facebookresearch/dinov2', 'dinov2_vitg14')
             self.embed_dim = 1536
-        self.patch_size = 14
+        self.patch_size = kwargs.get("patch_size", 14)
         # Send to device
         self.model.to(device)
         self.device = device
 
-        self._input_size = input_size
-        self._input_interp = input_interp
-        self.original_width = original_size[0]
-        self.original_height = original_size[1]
-        # Interpolation type
-        if self._input_interp == "bilinear":
-            self.interp = T.InterpolationMode.BILINEAR
-        elif self._input_interp == "nearest":
-            self.interp = T.InterpolationMode.NEAREST
-        elif self._input_interp == "bicubic":
-            self.interp = T.InterpolationMode.BICUBIC
-        self.center_crop=kwargs.get('center_crop', True)
         self.transform=self._create_transform()
 
     def _create_transform(self):
-        # Calculate aspect ratio preserving size
-        aspect_ratio = self.original_width / self.original_height
-        if aspect_ratio > 1:  # If width > height, scale by height (1280 -> 448)
-            new_height = self._input_size
-            new_width = int(new_height * aspect_ratio)
-            # check if new_width is a multiple of self.patch_size
-            if new_width % self.patch_size != 0:
-                new_width=new_width-new_width%self.patch_size
-        else:  # If height >= width, scale by width (1920 -> 448)
-            new_width = self._input_size
-            new_height = int(new_width / aspect_ratio)
-            # check if new_height is a multiple of self.patch_size
-            if new_height % self.patch_size != 0:
-                new_height=new_height-new_height%self.patch_size
-
         # Resize and then center crop to the expected input size
         transform = T.Compose([
-            T.Resize((new_height, new_width),self.interp),
-            T.CenterCrop(self._input_size) if self.center_crop else T.CenterCrop((new_height, new_width)),
-            T.ConvertImageDtype(torch.float),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
-        self.new_height=new_height
-        self.new_width=new_width
         return transform
     
     @torch.no_grad()
@@ -87,6 +52,12 @@ class Dinov2Interface:
         # print("After transform shape is:",img.shape)
         # Inference
         feat = self.model.forward_features(img)["x_norm_patchtokens"]
+        B=feat.shape[0]
+        C=feat.shape[2]
+        H=int(img.shape[2]/self.patch_size)
+        W=int(img.shape[3]/self.patch_size)
+        feat=feat.permute(0,2,1)
+        feat=feat.reshape(B,C,H,W)
         return feat
     
     def change_device(self, device):
@@ -101,12 +72,12 @@ class Dinov2Interface:
 if __name__=="__main__":
     import os
     import cv2
-
+    from PIL import Image
 
     # Initialize the interface with the desired device and parameters
     # Load an image and convert it to a PyTorch tensor
-    image_relative_path = 'image/hiking_2.jpeg'  # Update to the relative path of your image
-    feat_relative_path = 'image/hiking_2_feat.png'
+    image_relative_path = 'image/hiking.png'  # Update to the relative path of your image
+    feat_relative_path = 'image/hiking_feat.png'
     # Use os.path.join to get the full path of the image
     image_path = os.path.join(WVN_ROOT_DIR, image_relative_path)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -115,6 +86,9 @@ if __name__=="__main__":
     img = img.permute(2, 0, 1)
     img = (img.type(torch.float32) / 255)[None]
     
+    input_image = Image.open(image_path).convert('RGB')  # Convert to RGB if not already
+    input_tensor = T.ToTensor()(input_image)
+    img=input_tensor.unsqueeze(0)
     input_size = 1260
     # If you have a GPU with CUDA support, use 'cuda', otherwise 'cpu'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -139,18 +113,22 @@ if __name__=="__main__":
     import matplotlib.pyplot as plt
     import numpy as np
     from sklearn.decomposition import PCA
+    B,C,H,W=features.shape
+    features=features.permute(0,2,3,1)
+    features=features.reshape(B,H*W,C)
     features=features[0].cpu().numpy()
-    pca = PCA(n_components=3)
+    n=3
+    pca = PCA(n_components=n)
     pca.fit(features)
 
     pca_features = pca.transform(features)
-    for i in range(3):
+    for i in range(n):
         pca_features[:, i] = (pca_features[:, i] - pca_features[:, i].min()) / (pca_features[:, i].max() - pca_features[:, i].min())
     # pca_features = (pca_features - pca_features.min()) / (pca_features.max() - pca_features.min())
     pca_features = pca_features * 255
     feat_height = int(input_size / 14)
     feat_width = int(input_size/img.shape[-2]*img.shape[-1] / 14) 
-    plt.imshow(pca_features.reshape(feat_height, feat_width, 3).astype(np.uint8))
+    plt.imshow(pca_features.reshape(feat_height, feat_width, n).astype(np.uint8))
     image_path = os.path.join(WVN_ROOT_DIR, feat_relative_path)
     plt.savefig(image_path)
 
--- BaseWVN/utils/feat_extractor.py
+++ BaseWVN/utils/feat_extractor.py
@@ -1,7 +1,10 @@
 import torch
+import torch.nn.functional as F
 from dinov2_interface import Dinov2Interface
 import numpy as np
 from torchvision import transforms as T
+from typing import Union, Dict
+from BaseWVN import WVN_ROOT_DIR
 
 class FeatureExtractor:
     def __init__(
@@ -18,14 +21,27 @@ class FeatureExtractor:
         self._feature_type = feature_type
         self._input_size = input_size
         # input size must be a multiple of 14
-        assert self._input_size % 14 == 0, "Input size must be a multiple of 14"
+        self.patch_size = 14
+        assert self._input_size % self.patch_size == 0, "Input size must be a multiple of patch_size"
         # Extract original_width and original_height from kwargs if present
         self.original_width = kwargs.get('original_width', 1920)  # Default to 1920 if not provided
         self.original_height = kwargs.get('original_height', 1280)  # Default to 1080 if not provided
-        self.interp=kwargs.get('interp', 'bilinear')
+        self._input_interp=kwargs.get('interp', 'bilinear')
+        self.center_crop=kwargs.get('center_crop', False)
+
+        # Interpolation type
+        if self._input_interp == "bilinear":
+            self.interp = T.InterpolationMode.BILINEAR
+        elif self._input_interp == "nearest":
+            self.interp = T.InterpolationMode.NEAREST
+        elif self._input_interp == "bicubic":
+            self.interp = T.InterpolationMode.BICUBIC
+
+        self.transform=self._create_transform()
+
         # feature extractor
         if self._feature_type == "dinov2":
-            self.extractor=Dinov2Interface(device, input_size=self._input_size, original_size=(self.original_width,self.original_height), input_interp=self.interp)
+            self.extractor=Dinov2Interface(device, **kwargs)
         else:
             raise ValueError(f"Extractor[{self._feature_type}] not supported!")
         
@@ -39,18 +55,59 @@ class FeatureExtractor:
             )
         else:
             raise ValueError(f"Segmentation[{self._segmentation_type}] not supported!")
-         
+    
+    def extract(self, img, **kwargs):
+        """Extract features from image
+
+        Args:
+            img (torch.tensor): Image tensor
+
+        Returns:
+            sparse_features (torch.tensor, shape:(num_segs or H*W,C)): Sparse features tensor
+            seg (torch.tensor, shape:(H,W)): Segmentation map
+        """
+        img=self.transform(img)
+        # Compute segmentation
+        seg = self.compute_segments(img, **kwargs)
+        # Compute features
+        dense_features = self.compute_features(img, **kwargs)
+        # Sparsify features
+        sparse_features = self.sparsify_features(dense_features, seg)
+        return sparse_features, seg
+
     @property
     def feature_type(self):
         return self._feature_type
 
     @property
-    def feature_dim(self):
-        return self._feature_dim
-
-    @property
     def segmentation_type(self):
         return self._segmentation_type
+
+    def _create_transform(self):
+        # Calculate aspect ratio preserving size
+        aspect_ratio = self.original_width / self.original_height
+        if aspect_ratio > 1:  # If width > height, scale by height (1280 -> 448)
+            new_height = self._input_size
+            new_width = int(new_height * aspect_ratio)
+            # check if new_width is a multiple of self.patch_size
+            if new_width % self.patch_size != 0:
+                new_width=new_width-new_width%self.patch_size
+        else:  # If height >= width, scale by width (1920 -> 448)
+            new_width = self._input_size
+            new_height = int(new_width / aspect_ratio)
+            # check if new_height is a multiple of self.patch_size
+            if new_height % self.patch_size != 0:
+                new_height=new_height-new_height%self.patch_size
+
+        # Resize and then center crop to the expected input size
+        transform = T.Compose([
+            T.Resize((new_height, new_width),self.interp),
+            T.CenterCrop(self._input_size) if self.center_crop else T.CenterCrop((new_height, new_width)),
+            T.ConvertImageDtype(torch.float),
+        ])
+        self.new_height=new_height
+        self.new_width=new_width
+        return transform
 
     def change_device(self, device):
         """Changes the device of all the class members
@@ -80,3 +137,175 @@ class FeatureExtractor:
         img_np = img.cpu().numpy()
         seg = self.slic.iterate(np.uint8(np.ascontiguousarray(img_np) * 255))
         return torch.from_numpy(seg).to(self._device).type(torch.long)
+    
+    def compute_features(self, img: torch.tensor, **kwargs):
+        img_internal=img.clone()
+        if self._feature_type=="dinov2":
+            feat=self.extractor.inference(img_internal)
+        else:
+            raise ValueError(f"Extractor[{self._feature_type}] not supported!")
+        return feat
+    
+    def sparsify_features(self, dense_features: Union[torch.Tensor, Dict[str, torch.Tensor]], seg: torch.tensor, cumsum_trick=False):
+        """ Sparsify features
+         
+        return (H*W/segs_num,C)
+          
+            """
+        if isinstance(dense_features, dict):
+            # Multiscale feature pyramid extraction
+            scales_x = [  seg.shape[0]/feat.shape[2] for feat in dense_features.values()]
+            scales_y = [ seg.shape[1] /feat.shape[3] for feat in dense_features.values()]
+            # upsampling the feat of each scale
+            resized_feats = [
+                F.interpolate(
+                    feat.type(torch.float32), scale_factor=(scale_x, scale_y)
+                )
+                for scale_x, scale_y,feat in zip(scales_x, scales_y,dense_features.values())
+            ]
+            sparse_features = []
+
+            for i in range(seg.max() + 1):
+                single_segment_feature = []
+                for resized_feat in resized_feats:
+                    m=seg==i
+                    x, y = torch.where(m)
+                    avg_feat_per_seg = torch.mean(resized_feat[:, :, x, y], dim=-1)
+                    single_segment_feature.append(avg_feat_per_seg)
+                single_segment_feature = torch.cat(single_segment_feature, dim=1)
+                sparse_features.append(single_segment_feature)
+            
+        else:
+            # check if las two dim of seg is equal to dense_features dim, if not ,resize the feat
+            if seg.shape[-2:] != dense_features.shape[-2:]:
+                dense_features = F.interpolate(
+                    dense_features.type(torch.float32), size=seg.shape[-2:]
+                )
+            # Sparsify features
+            sparse_features = []
+            if self._segmentation_type != "pixel":
+                for i in range(seg.max() + 1):
+                    m = seg == i
+                    x, y = torch.where(m)
+                    avg_feat_per_seg = torch.mean(dense_features[:, :, x, y], dim=-1)
+                    sparse_features.append(avg_feat_per_seg)
+            else:
+                dense_features=dense_features.permute(0,2,3,1)
+                sparse_features = dense_features.reshape(dense_features.shape[0],dense_features.shape[1]*dense_features.shape[2],-1)
+                return sparse_features
+        # Concatenate features
+        
+        sparse_features = torch.stack(sparse_features, dim=1)
+        return sparse_features
+
+def test_extractor():
+    import cv2
+    import os
+    image_relative_path = 'image/hiking.png'  # Update to the relative path of your image
+    feat_relative_path = 'image/hiking_feat_low.png'
+    # Use os.path.join to get the full path of the image
+    image_path = os.path.join(WVN_ROOT_DIR, image_relative_path)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    np_img = cv2.imread(image_path)
+    img = torch.from_numpy(cv2.cvtColor(np_img, cv2.COLOR_BGR2RGB)).to(device)
+    img = img.permute(2, 0, 1)
+    img = (img.type(torch.float32) / 255)[None]
+    input_size=1260
+    extractor=FeatureExtractor(device, input_size=input_size, original_width=img.shape[-1], original_height=img.shape[-2], interp='bilinear')
+    features, seg=extractor.extract(img)
+    
+    
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from sklearn.decomposition import PCA
+    B,N,C=features.shape
+    features=features[0].cpu().numpy()
+    # B,C,H,W=features.shape
+    # features=features.permute(0,2,3,1)
+    # features=features.reshape(B,H*W,C)
+    # features=features[0].cpu().numpy()
+    n=3
+    pca = PCA(n_components=n)
+    pca.fit(features)
+
+    pca_features = pca.transform(features)
+    for i in range(n):
+        pca_features[:, i] = (pca_features[:, i] - pca_features[:, i].min()) / (pca_features[:, i].max() - pca_features[:, i].min())
+    # pca_features = (pca_features - pca_features.min()) / (pca_features.max() - pca_features.min())
+    pca_features = pca_features * 255
+    feat_height = seg.shape[0]
+    feat_width = seg.shape[1]
+    plt.imshow(pca_features.reshape(feat_height, feat_width, n).astype(np.uint8))
+    image_path = os.path.join(WVN_ROOT_DIR, feat_relative_path)
+    plt.savefig(image_path)
+
+
+
+                
+if __name__=="__main__":
+    import torch
+    import torch.nn.functional as F
+
+    # Original segmentation map
+    seg = torch.tensor([
+    [0, 0, 1, 1],
+    [0, 0, 1, 1],
+    [2, 2, 3, 3],
+    [2, 2, 3, 3]
+    ])
+    print(seg.reshape(-1))
+    # Scale factors
+    scales_x = [2, 0.5]
+    scales_y = [2, 0.5]
+
+    # Resized segmentation maps
+    segs = [
+        F.interpolate(
+            seg[None, None, :, :].type(torch.float32), scale_factor=(scale_x, scale_y), mode='nearest'
+        )[0, 0].type(torch.long)
+        for scale_x, scale_y in zip(scales_x, scales_y)
+    ]
+
+    # Print the resized maps
+    for i, resized_seg in enumerate(segs):
+        print(f"Resized seg at scale {scales_x[i]}x{[scales_y[i]]}:")
+        print(resized_seg.numpy(), "\n")
+    
+    dense_features = {
+        "feat_1":torch.rand((8, 4, 4)).unsqueeze(0),
+        "feat_2":torch.rand((16, 2, 2)).unsqueeze(0),
+
+    }
+
+    test_extractor()
+
+    # if isinstance(dense_features, dict):
+    #     # Multiscale feature pyramid extraction
+    #     scales_x = [  seg.shape[0]/feat.shape[2] for feat in dense_features.values()]
+    #     scales_y = [ seg.shape[1] /feat.shape[3] for feat in dense_features.values()]
+    #     # upsampling the feat of each scale
+    #     resized_feats = [
+    #         F.interpolate(
+    #             feat.type(torch.float32), scale_factor=(scale_x, scale_y)
+    #         )
+    #         for scale_x, scale_y,feat in zip(scales_x, scales_y,dense_features.values())
+    #     ]
+    #     sparse_features = []
+
+    #     for i in range(seg.max() + 1):
+    #         single_segment_feature = []
+    #         for resized_feat in resized_feats:
+    #             m=seg==i
+    #             x, y = torch.where(m)
+    #             avg_feat_per_seg = torch.mean(resized_feat[:, :, x, y], dim=-1)
+    #             single_segment_feature.append(avg_feat_per_seg)
+    #         single_segment_feature = torch.cat(single_segment_feature, dim=1)
+    #         sparse_features.append(single_segment_feature)
+    #     # check if feat dim is 4
+    #     if len(sparse_features[0].shape)==2:
+    #         sparse_features = torch.cat(sparse_features, dim=0)
+    #     else:
+    #         sparse_features = torch.stack(sparse_features, dim=0)
+    #     pass
+
+  
