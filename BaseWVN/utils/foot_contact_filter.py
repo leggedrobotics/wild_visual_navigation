fe5c546e16c4315d551d858876e1e92af3a9a4d0
swsychen <swsychen@gmail.com>
2023-11-23 20:20:49 +0100

    frequency filter only

--- BaseWVN/utils/foot_contact_filter.py
+++ BaseWVN/utils/foot_contact_filter.py
@@ -13,12 +13,15 @@ class FootFilter:
         self.last_contact_time=None
         self.last_contact_position = None
         self.max_xy_distance=0.1
+        self.continuous_contact_count = 0
+        self.contact_threshold = 8 
     
-    
-    def filter(self, current_pose, estimated_contact: Optional[bool] = None, current_time: float = None):
+    def filter_to_single(self, current_pose, estimated_contact: Optional[bool] = None, current_time: float = None):
         """
         current_pose: [x, y, z, rx, ry, rz, rw]
         Filters the foot contact state from a noisy state estimator using a temporal method.
+        It'll preserve only one contact state in a sequence of contact states.
+        Not useful in my case.
         """
  
         # Add to the buffer
@@ -26,8 +29,15 @@ class FootFilter:
         if len(self.poses) > self.max_poses_window:
             self.poses = self.poses[-self.max_poses_window:]
             self.filtered_poses = self.filtered_poses[-self.max_poses_window:]
-            # plot_foot_data(self)
+            plot_foot_data(self)
 
+        filtered_contact = False
+        if estimated_contact:
+            self.continuous_contact_count += 1
+            if self.continuous_contact_count >= self.contact_threshold:
+                filtered_contact = True
+        else:
+            self.continuous_contact_count = 0
         # Check for the duration since last contact
         if estimated_contact and self.last_contact_position is not None:
             time_since_last_contact = current_time - self.last_contact_time
@@ -46,17 +56,50 @@ class FootFilter:
                 self.poses.append((current_pose, estimated_contact, current_time))
                 self.filtered_poses.append((current_pose, False, current_time))
                 return False 
-
-            
+  
         # Update last contact details if current state is a contact
-        if estimated_contact:
+        if filtered_contact:
             self.last_contact_time = current_time
             self.last_contact_position = current_pose
-        self.filtered_poses.append((current_pose, estimated_contact, current_time))
+        self.filtered_poses.append((current_pose, filtered_contact, current_time))
         self.poses.append((current_pose, estimated_contact, current_time))
 
             
-        return estimated_contact
+        return filtered_contact
+
+    def filter(self, current_pose, estimated_contact: Optional[bool] = None, current_time: float = None):
+        """
+        current_pose: [x, y, z, rx, ry, rz, rw]
+        Filters the foot contact state from a noisy state estimator using a temporal method.
+        This one will preserve most contact states in a sequence of contact states.
+        """
+
+        # Add to the buffer
+        current_pose = np.array(current_pose)
+        if len(self.poses) > self.max_poses_window:
+            self.poses = self.poses[-self.max_poses_window:]
+            self.filtered_poses = self.filtered_poses[-self.max_poses_window:]
+            # plot_foot_data(self)
+
+        # Sliding window for the contact states
+        window_size = 5
+        contact_threshold = 3  # Minimum number of contacts in the window to consider as valid contact
+        contact_window = [pose[1] for pose in self.poses[-window_size:]]  # Get the last 10 contact states
+        # Check if the number of contacts in the window meets or exceeds the threshold
+        if contact_window.count(True) >= contact_threshold and estimated_contact:
+            filtered_contact = True
+        else:
+            filtered_contact = False
+
+        # Update last contact details if current state is a contact
+        if filtered_contact:
+            self.last_contact_time = current_time
+            self.last_contact_position = current_pose
+        self.filtered_poses.append((current_pose, filtered_contact, current_time))
+        self.poses.append((current_pose, estimated_contact, current_time))
+
+            
+        return filtered_contact
 
 def plot_foot_data(foot_filter:FootFilter):
     poses=foot_filter.filtered_poses
