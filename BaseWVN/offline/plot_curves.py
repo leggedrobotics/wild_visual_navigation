1b2a79aaf8d0b91e1a7c9c9ccf4d6c11f5ba8fdb
swsychen <swsychen@gmail.com>
2023-11-30 16:42:51 +0100

    add color palette for plot

--- BaseWVN/offline/plot_curves.py
+++ BaseWVN/offline/plot_curves.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from BaseWVN import WVN_ROOT_DIR
 from matplotlib import gridspec
 # Parent directory containing the metric folders
-parent_folder = os.path.join(WVN_ROOT_DIR, 'results/compare')
+parent_folder = os.path.join(WVN_ROOT_DIR, 'results/compare/PretrainorNot')
+color_palette = ['red', 'blue', 'green', 'orange', 'purple', 'brown']
 
 # List of metric names corresponding to the folder names
 metrics = [name for name in os.listdir(parent_folder) if os.path.isdir(os.path.join(parent_folder, name))]
@@ -34,19 +35,20 @@ for i, row in enumerate(row_metrics):
         folder_path = os.path.join(parent_folder, metric)
         csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
-        for file in csv_files:
+        for file_idx, file in enumerate(csv_files):
             file_path = os.path.join(folder_path, file)
             df = pd.read_csv(file_path, header=None)
             steps = df.iloc[:, 0]
             values = df.iloc[:, -1]
             label = file.rsplit('.csv', 1)[0]
+            color = color_palette[file_idx % len(color_palette)]  # Cycle through the color palette
             if metric == 'Overconfidence_mean':
                 overconfidence_data.append(values)
                 overconfidence_files.append(label)
             elif metric == 'Underconfidence_mean':
                 underconfidence_data.append(values)
                 underconfidence_files.append(label)
-            ax.plot(steps, values, label=label)
+            ax.plot(steps, values, label=label,color=color)  # label using filename
             ax.set_xlabel('Step')
             ax.set_ylabel(metric if 'confidence' not in metric else f'{metric} (%)')
             ax.set_title(f'{metric} Metric' if 'confidence' not in metric else f'{metric} Metric (Values in %)')
@@ -55,9 +57,10 @@ for i, row in enumerate(row_metrics):
 # Process Confidence Mask Accuracy if both metrics are present
 if overconfidence_data and underconfidence_data:
     ax = plt.subplot(gs[3, :])  # Span all columns in the last row
-    for o_data, u_data, o_label in zip(overconfidence_data, underconfidence_data, overconfidence_files):
+    for o_data, u_data, o_label, idx in zip(overconfidence_data, underconfidence_data, overconfidence_files, range(len(overconfidence_files))):
         cma = [100 - o - u for o, u in zip(o_data, u_data)]
-        ax.plot(steps, cma, label=f'{o_label}')  # label using overconfidence filename
+        color = color_palette[idx % len(color_palette)]
+        ax.plot(steps, cma, label=f'{o_label}',color=color)  # label using overconfidence filename
 
     ax.set_xlabel('Step')
     ax.set_ylabel('Confidence Mask Accuracy (%)')
