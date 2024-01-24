c7aff7484addea3c8c6bae6bd93681c81921e560
RobinSchmid7 <schmirob@student.ethz.ch>
2024-01-24 15:30:05 +0100

    Adding dinov2 features

--- wild_visual_navigation/feature_extractor/__init__.py
+++ wild_visual_navigation/feature_extractor/__init__.py
@@ -1,4 +1,5 @@
 from .dino_interface import DinoInterface, run_dino_interfacer
+from .dino2_interface import Dino2Interface
 from .torchvision_interface import TorchVisionInterface
 
 # from .dino_trt_interface import DinoTrtInterface, TrtModel, run_dino_trt_interfacer
